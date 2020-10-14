c094aad8c3412d522b742c1265c9cd96be6413b6
Karim ElSharkawy <k_elsharkawy@hotmail.com>
2020-10-15 01:23:23 +0200

    Converted to Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  Loggedin = false;
   title = 'foodiefood';
+
+  isLoggedin() {
+    if (this.Loggedin) {
+      return true;
+    } else return false;
+  }
 }
